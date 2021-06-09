846b4a2aede6c4e9dfb00fcfeba57695a802c41a
Scareships <Scareships@users.noreply.github.com>
2021-06-09 18:34:07 +0200

    hack made LoL but BUGS

--- Assoult Cube Hack/dllmain.cpp
+++ Assoult Cube Hack/dllmain.cpp
@@ -1,20 +1,64 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+#include "functions.h"
+
+bool healthSwitch = false;
+DWORD moduleBase = (DWORD)GetModuleHandle("ac_client.exe");
+DWORD localPlayer = *(DWORD*)(moduleBase + 0x10F4F4);
+DWORD WINAPI MainThread(HMODULE hModule)
+{
+
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    printf("Injected!\n[F1] Unlimited health\n[END] Exit!\n");
+    while (true)
+    {
+        // Switch
+        if (GetAsyncKeyState(VK_F1) & 1)
+        {
+            healthSwitch = !healthSwitch;
+            if (healthSwitch == true)
+            {
+                printf("Unlimited health on!\n");
+
+            }
+            else
+            {
+                printf("Unlimited health off!\n");
+            }
+
+        }
+
+        if (healthSwitch == true)
+        {
+            //Write our health value to 1337
+            *(int*)(localPlayer + 0xF8) = 1337;
+        }
+
+        if (GetAsyncKeyState(VK_END) & 1)
+        {
+            fclose(f);
+            FreeConsole();
+            FreeLibraryAndExitThread(hModule, NULL);
+        }
+    }
+
+    return 0;
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MainThread, hModule, NULL, NULL);
         break;
-        //test
     }
     return TRUE;
 }
-
